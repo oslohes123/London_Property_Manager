57cbfc6bd9869ecb54d6475463b25e1a4b0674ef
Yousef <Yousef.a.altaher@gmail.com>
2022-03-09 10:36:55 +0000

    Configured the map

--- London_Property_Market/src/main/java/com/example/london_property_market/UI/MainViewer.java
+++ London_Property_Market/src/main/java/com/example/london_property_market/UI/MainViewer.java
@@ -1,21 +1,26 @@
 package com.example.london_property_market.UI;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
-import com.esri.arcgisruntime.data.ServiceFeatureTable;
-import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.*;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class MainViewer extends Application {
+
+    private final double LONDON_LONGITUDE = -0.14130290092735798;
+    private final double LONDON_LATITUDE = 51.493866432732425;
+    private final double MAP_SCALE = 472223.819286;
 
     private MapView mapView;
 
@@ -30,10 +35,17 @@ public class MainViewer extends Application {
         ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_NAVIGATION);
 
         mapView.setMap(map);
+        mapView.setEnableMousePan(false);
+        mapView.setEnableMouseZoom(false);
+        mapView.setEnableKeyboardNavigation(false);
+        mapView.setEnableTouchPan(false);
+        mapView.setEnableTouchRotate(false);
+        mapView.setEnableTouchZoom(false);
 
-        mapView.setViewpoint(new Viewpoint(34.027, -118.805, 72223.819286));
+        mapView.setViewpoint(new Viewpoint(LONDON_LATITUDE, LONDON_LONGITUDE, MAP_SCALE));
+        mapView.setOnMouseClicked(this::mouse);
 
-        Scene scene = new Scene(new BorderPane(), 320, 240);
+        Scene scene = new Scene(new BorderPane(mapView), 1020, 740);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -41,6 +53,13 @@ public class MainViewer extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private void mouse(MouseEvent mouseEvent) {
+        Point2D point = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+        Point mapPoint = mapView.screenToLocation(point);
+        Point projectedPoint = (Point) GeometryEngine.project(mapPoint, SpatialReferences.getWgs84()) ;
+        System.out.println(projectedPoint.getX() + " " + projectedPoint.getY());
     }
 
     /**
