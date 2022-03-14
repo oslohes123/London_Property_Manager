c49eabbfe947055fa44eb6dc37eba0a45df8866e
Shaheer Effandi <shaheer167@hotmail.com>
2022-03-14 20:07:04 +0000

    "Started Work on giving button functionality"

--- src/main/java/com/example/london_property_market/UI/MainViewer.java
+++ src/main/java/com/example/london_property_market/UI/MainViewer.java
@@ -1,18 +1,32 @@
 package com.example.london_property_market.UI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 
 import java.io.IOException;
 
 public class MainViewer extends Application {
+
+    @FXML
+    private Button goMapButton;
+
+
+    @FXML
+    public void goMap(ActionEvent event)
+    {
+        System.out.println("Printed out thing");
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         File file = new File("resources/welcome2.fxml");
