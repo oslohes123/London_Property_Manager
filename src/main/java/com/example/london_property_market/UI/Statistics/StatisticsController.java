1cbe3d7ff585573a50a41600e6d4f873b19461fe
AshleyT <61867169+oslohes123@users.noreply.github.com>
2022-03-18 13:05:10 +0000

    Added stuff to statscontroller

--- src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
+++ src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
@@ -1,4 +1,29 @@
 package com.example.london_property_market.UI.Statistics;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import java.awt.*;
+
 public class StatisticsController {
+
+    @FXML
+    public Button leftStatsButton = new Button();
+    @FXML
+    public Button rightStatsButton = new Button();
+    @FXML
+    public Label statsDescription;
+    @FXML
+    public Label statsTitle;
+
+
+    @FXML
+    public void leftButtonClick(MouseEvent mouseEvent) {
+        
+    }
+
+    @FXML
+    public void rightButtonClick(MouseEvent mouseEvent) {
+    }
 }
