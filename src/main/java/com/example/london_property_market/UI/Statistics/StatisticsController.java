a9282df8c16d9b950f0609943379a7b250cab3ab
AshleyT <61867169+oslohes123@users.noreply.github.com>
2022-03-18 13:20:26 +0000

    Added statTable for each borough

--- src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
+++ src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
@@ -2,6 +2,7 @@ package com.example.london_property_market.UI.Statistics;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 import java.awt.*;
@@ -13,9 +14,8 @@ public class StatisticsController {
     @FXML
     public Button rightStatsButton = new Button();
     @FXML
-    public Label statsDescription;
-    @FXML
     public Label statsTitle;
+    public TableView statTable;
 
 
     @FXML
