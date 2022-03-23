16a3248a478af4c68637b2c587860dda4cd3c7f9
Ashley Tyagi <ashleytyagi@hotmail.com>
2022-03-23 12:32:56 +0000

    Added data to be added to the tableview
    This fixes previous issues where data wasn't being added

--- src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
+++ src/main/java/com/example/london_property_market/UI/Statistics/StatisticsController.java
@@ -3,6 +3,7 @@ package com.example.london_property_market.UI.Statistics;
 import com.example.london_property_market.UI.Welcome.MainModel;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,6 +18,7 @@ import javafx.util.Callback;
 import java.awt.*;
 import java.net.URL;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,13 +34,14 @@ public class StatisticsController implements Initializable {
     public Label statsTitle;
     @FXML
     public TableView statTable;
+    private ObservableList<ObservableList> data;
 
     private StatisticsModel statModel;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 
     public void setBoroughsList(List<String> boroughsList){
@@ -61,24 +64,51 @@ public class StatisticsController implements Initializable {
         setupLabels(rs);
     }
 
+    /**
+     * Answer takes some inspiration from
+     * https://stackoverflow.com/questions/18941093/how-to-fill-up-a-tableview-with-database-data
+     * @param rs
+     */
     private void setupLabels(ResultSet rs){
         String label = statModel.getStatName();
         statsTitle.setText(label);
+        data = FXCollections.observableArrayList();
         try{
             statTable.getColumns().clear();
-            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            // This part is for adding the column names
+            for(int i=0 ; i< rsmd.getColumnCount(); i++){
                 //We are using non property style for making dynamic table
                 final int j = i;
 
-                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                TableColumn col = new TableColumn(rsmd.getColumnName(i+1));
                 col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
 
                 statTable.getColumns().addAll(col);
-                System.out.println("Column ["+i+"] ");
+//                System.out.println("Column ["+i+"] ");
+
+            }
+
+            // This part is for adding the data to each of the columns
+            while(rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+//                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+                statTable.setItems(data);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+
     }
 
 }
